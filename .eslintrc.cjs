660f5d24fc4223cc613731bc0339452ca526ce9f
ElGoel <dazhkhors@gmail.com>
2023-05-02 12:59:18 -0500

    create and configure a vite + typescript + vue project, eslint/prettier install and configure done, sass and 7-1 style done, dependencies required install done

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'prettier',
+    'eslint-config-prettier',
+  ],
+  overrides: [],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier', 'eslint-comments'],
+  settings: {
+    'eslint.validate': ['javascript', 'typescript'],
+  },
+  rules: {
+    'no-empty': 'error',
+    camelcase: 'error',
+    'spaced-comment': 'error',
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    'prettier/prettier': 'error',
+  },
+};
